aad44cf690f36b0e1560a075ae7ea96fadc534de
anugrah3199 <anugrahdixit93@gmail.com>
2022-01-23 16:18:05 +0530

    fix: sort

--- task1/app.js
+++ task1/app.js
@@ -14,11 +14,17 @@ const getproducts = async() => {
 
 async function renderproducts() {
     const data = await getproducts();
+    console.log(data)
     const keys = Object.keys(data);
+    const arr = []
+    keys.forEach((ele) => {
+        arr.push(data[ele])
+    })
+    arr.sort((a, b) => b.popularity - a.popularity)
 
-
-    keys.forEach((key, index) => {
-        const product = data[key]
+    arr.forEach((index) => {
+        const product = index
+        console.log(product)
         let row = table.insertRow();
         for (ele in product) {
             let cell = row.insertCell();
